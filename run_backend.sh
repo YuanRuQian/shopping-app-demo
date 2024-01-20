9dfab02c58721d9dea309b638c612d7bcb15e744
Lydia Yuan <lydiayuan99@gmail.com>
2024-01-20 12:09:14 -0700

    add: shell script to run backend

---
+++ run_backend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar OnlineShopping-0.0.1-jar-with-dependencies.jar
--- shoppingappdemo/app/src/main/java/yuan/lydia/shoppingappdemo/MainActivity.kt
+++ shoppingappdemo/app/src/main/java/yuan/lydia/shoppingappdemo/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Modifier
 import yuan.lydia.shoppingappdemo.ui.screens.AppCanvas
 import yuan.lydia.shoppingappdemo.ui.theme.ShoppingappdemoTheme
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
